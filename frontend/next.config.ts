87c5ea4d78b03d83ea0e0ae82aef79af32fbea96
relmoo1220 <gamingwithmura@gmail.com>
2025-02-14 21:48:51 +0800

    gh pages

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,20 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Disable default image optimization
+  },
+  assetPrefix: isProd ? "/your-repository-name/" : "",
+  basePath: isProd ? "/your-repository-name" : "",
+  output: "export",
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
